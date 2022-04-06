a68ef73761fa20b2ae1a5402671d4e59c09b606b
LiuChangFreeman <liuchangfreeman@live.com>
2022-04-06 18:36:50 +0800

    fix bug

---
+++ C--Compiler/Model/Storage.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows;
+using Windows.Storage;
+using Windows.ApplicationModel;
+
+namespace Compiler.Model
+{
+    class Storage
+    {
+        private static string storagePath = ApplicationData.Current.LocalCacheFolder.Path;
+        //private static string storagePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),"C--Compiler");
+
+        public static string getEditorPath()
+        {
+            string editorFolder;
+            try
+            {
+                editorFolder = Path.Combine(Package.Current.InstalledLocation.Path, "C--Compiler", "Editor");
+                if (!Directory.Exists(editorFolder))
+                {
+                    editorFolder = Path.Combine(Package.Current.InstalledLocation.Path, "Editor");
+                }
+            }
+            catch (Exception)
+            {
+                editorFolder = Path.Combine(Environment.CurrentDirectory, "Editor");
+            }
+            return string.Format("file:///{0}/index.html", editorFolder);
+        }
+
+        public static string getStoragePath(string path)
+        {
+            return Path.Combine(storagePath, path);
+        }
+
+        public static Task ensureAsserts()
+        {
+            //if (!Directory.Exists(storagePath))
+            //{
+            //    Directory.CreateDirectory(storagePath);
+            //}
+            string descFolder = getStoragePath("Source");
+            if (!Directory.Exists(descFolder))
+            {
+                string sourceFolder;
+                try
+                {
+                    sourceFolder = Path.Combine(Package.Current.InstalledLocation.Path,"C--Compiler", "Source");
+                    if (!Directory.Exists(sourceFolder))
+                    {
+                        sourceFolder = Path.Combine(Package.Current.InstalledLocation.Path, "Source");
+                    }
+                }
+                catch (Exception)
+                {
+                    sourceFolder = Path.Combine(Environment.CurrentDirectory, "Source");
+                }
+                CopyFolder(sourceFolder, descFolder);
+            }
+
+            return Task.CompletedTask;
+        }
+
+        private static void CopyFolder(string sourceFolder, string destFolder)
+        {
+            try
+            {
+                if (!Directory.Exists(destFolder))
+                {
+                    Directory.CreateDirectory(destFolder);
+                }
+                string[] files = Directory.GetFiles(sourceFolder);
+                foreach (string file in files)
+                {
+                    string name = Path.GetFileName(file);
+                    string dest = Path.Combine(destFolder, name);
+                    File.Copy(file, dest);
+                }
+                string[] folders = Directory.GetDirectories(sourceFolder);
+                foreach (string folder in folders)
+                {
+                    string dirName = folder.Split('\\')[folder.Split('\\').Length - 1];
+                    string destfolder = Path.Combine(destFolder, dirName);
+                    CopyFolder(folder, destfolder);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"copy file Error:{ex.Message}\r\n source:{ex.StackTrace}");
+            }
+        }
+    }
+}
---
+++ C--Compiler/View/MiddleCode.xaml.cs
@@ -0,0 +1,77 @@
+﻿using Compiler.Model;
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Microsoft.Web.WebView2.Core;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Compiler.View
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MiddleCode : Window
+    {
+        public MiddleCode()
+        {
+            this.InitializeComponent();
+            InitWindow();
+        }
+
+        private void InitWindow()
+        {
+            IntPtr hWnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+            WindowId windowId = Win32Interop.GetWindowIdFromWindow(hWnd);
+            AppWindow appWindow = AppWindow.GetFromWindowId(windowId);
+            var size = new Windows.Graphics.SizeInt32();
+            size.Width = 710;
+            size.Height = 850;
+            appWindow.Resize(size);
+            appWindow.SetIcon("icon.ico");
+            this.Title = "中间代码";
+            this.Closed += (o, e) =>
+            {
+                MainWebView.Close();
+            };
+            LoadedWebview();
+        }
+
+        private async void LoadedWebview()
+        {
+            var sourceFolder = Storage.getStoragePath("Source");
+            await MainWebView.EnsureCoreWebView2Async();
+            var coreWebView = MainWebView.CoreWebView2;
+            coreWebView.Settings.IsZoomControlEnabled = false;
+            coreWebView.Settings.AreDevToolsEnabled = true;
+            coreWebView.Navigate(Storage.getEditorPath());
+            coreWebView.WebMessageReceived += async (s, e) =>
+            {
+                message msg = JsonConvert.DeserializeObject<message>(e.TryGetWebMessageAsString());
+                if (msg.type == "init_code")
+                {
+                    string middleFilePath = Path.Combine(sourceFolder, "result.middle");
+                    string middleFileSource = await File.ReadAllTextAsync(middleFilePath);
+                    msg.value = middleFileSource;
+                    coreWebView.PostWebMessageAsString(JsonConvert.SerializeObject(msg));
+                }
+            };
+        }
+    }
+}
